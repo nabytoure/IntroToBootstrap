d1c020eb1c492acfb6c83b405dab0f9a07d5cad5
nabytoure <nabytoure@yahoo.fr>
2014-04-25 11:03:44 -0400

    commit of IntroTobootstrap project
    Description of the commit of IntroTobootstrap project by 25 April

---
+++ IntroToBootstrap/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace IntroToBootstrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+            /*
+            ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 
+            |-------------------------------Commentaire entete-------------------------------|
+            ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 
+            */
+
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
